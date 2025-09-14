e4ec8c2e346a1e69475cec0acc674ab24c1f206a
nipun221 <nipunvats5@gmail.com>
2025-09-14 12:07:08 +0530

    Implemented string feature

--- test.py
+++ test.py
@@ -9,3 +9,9 @@ print(a * 2)
 # Multi-dimensional array
 res = np.array([[1, 2], [3, 4]])
 print(res * 2)
+
+s = "hello geeks"
+s1 = "H" + s[1:]                   # update first character
+s2 = s.replace("geeks", "GeeksforGeeks")  # replace word
+print(s1)
+print(s2)
