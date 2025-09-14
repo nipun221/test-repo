2c329e22cd110e9ebe9d2bad49a4201d482ade9c
Nipun Vats <nipunvats5@gmail.com>
2025-09-14 11:46:35 +0530

    Update test.py - array code addes

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 print("Hello world")
 
+import numpy as np
+a = np.array([1, 2, 3, 4])
+
+# Element-wise operations
+print(a * 2)  
+
+# Multi-dimensional array
+res = np.array([[1, 2], [3, 4]])
+print(res * 2)
